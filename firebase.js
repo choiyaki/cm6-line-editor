d82c3e61467f795c4ccec9456e302d52a02cc398
吉田篤史 <choiyaki@gmail.com>
2026-02-14 17:26:31 +0900

    apiを再発行

--- firebase.js
+++ firebase.js
@@ -16,13 +16,14 @@ import {
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOX6A5veapHTN_7ma8qgP7-kstDIIV-y8",
+	apiKey: "AIzaSyCOX6A5veapHTN_7ma8qgP7-kstDIIV-y8",
   authDomain: "cm6-line-editor.firebaseapp.com",
   projectId: "cm6-line-editor",
   storageBucket: "cm6-line-editor.firebasestorage.app",
   messagingSenderId: "1016509709083",
-  appId: "1:1016509709083:web:bbe88605f3d9dbfa4d31be",
-  measurementId: "G-7YQNKGV5XG"
+  appId: "1:1016509709083:web:2bf7494dcbd330964d31be",
+  measurementId: "G-4RCKETB99Z"
+
 };
 
 // Initialize Firebase
